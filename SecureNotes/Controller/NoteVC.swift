61574913a5d89f22ad288f8c299a4c6b7a3511f5
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-08 20:42:32 +0200

    lesson1 noteVC setup

---
+++ SecureNotes/Controller/NoteVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SecureNotes
+//
+//  Created by Molnár Csaba on 2019. 09. 08..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class NoteVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension NoteVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
